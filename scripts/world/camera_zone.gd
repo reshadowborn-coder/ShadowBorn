4f7f86c4480704439a5be2da39e1b77e16d9e351
reshadowborn-coder <re.shadowborn@gmail.com>
2026-09-25 01:03:06 +0200

    Fix CameraZone native priority conflict

--- scripts/world/camera_zone.gd
+++ scripts/world/camera_zone.gd
@@ -4,4 +4,4 @@ extends Area3D
 @export var camera_position := Vector3(0, 9, 11)
 @export var camera_rotation := Vector3(-28, 0, 0)
 @export var fov := 42.0
-@export var priority := 0
+@export var zone_priority := 0
