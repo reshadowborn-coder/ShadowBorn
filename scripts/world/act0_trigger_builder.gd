32a76cc60d0aa10cf57e879e257d27d8bb47bb81
reshadowborn-coder <re.shadowborn@gmail.com>
2026-09-25 09:44:45 +0200

    Make Temple progression handoffs unavoidable

--- scripts/world/act0_trigger_builder.gd
+++ scripts/world/act0_trigger_builder.gd
@@ -4,12 +4,12 @@ extends Node3D
 func _ready()->void:
 	_gate(Act0Layout.TEMPLE_GATE_TRIGGER,"TempleGate","res://scripts/world/temple_gate.gd",Act0Layout.TEMPLE_GATE_TRIGGER_SIZE)
 	_gate(Act0Layout.FADED_SIGIL_TRIGGER,"FadedSigil","res://scripts/world/faded_sigil_threshold.gd",Act0Layout.FADED_SIGIL_TRIGGER_SIZE)
-	_interact(Act0Layout.KEEPER_TRIGGER,"Keeper","keeper")
-	_interact(Act0Layout.COVENANT_TRIGGER,"Covenant","covenant")
+	_interact(Act0Layout.KEEPER_TRIGGER,"Keeper","keeper",Act0Layout.KEEPER_TRIGGER_SIZE)
+	_interact(Act0Layout.COVENANT_TRIGGER,"Covenant","covenant",Act0Layout.COVENANT_TRIGGER_SIZE)
 	_interact(Act0Layout.SMITH_TRIGGER,"Smith","smith")
 	_interact(Act0Layout.MERCHANT_TRIGGER,"Merchant","merchant")
 	_interact(Act0Layout.ENGRAVER_TRIGGER,"Engraver","engraver")
-	_interact(Act0Layout.CATACOMBS_TRIGGER,"CatacombsEntry","catacombs")
+	_interact(Act0Layout.CATACOMBS_TRIGGER,"CatacombsEntry","catacombs",Act0Layout.CATACOMB_ENTRY_TRIGGER_SIZE)
 	for room in range(1,6):
 		_room(Act0Layout.catacomb_room_trigger_position(room),"CatacombRoom%d"%room,room)
 
@@ -29,8 +29,8 @@ func _gate(pos:Vector3,name:String,script_path:String,size:Vector3)->void:
 	a.set_script(load(script_path))
 	add_child(a)
 
-func _interact(pos:Vector3,name:String,kind:String)->void:
-	var a:=_area(name,pos)
+func _interact(pos:Vector3,name:String,kind:String,size:Vector3=Act0Layout.TEMPLE_INTERACTION_TRIGGER_SIZE)->void:
+	var a:=_area(name,pos,size)
 	a.set_script(load("res://scripts/world/temple_interaction.gd"))
 	a.set("interaction",kind)
 	add_child(a)
