ed16df7556c9ce8ce10dad4aa783fc492dea61bf
reshadowborn-coder <re.shadowborn@gmail.com>
2026-09-25 12:19:41 +0200

    Preload fixed Act 0 trigger scripts

--- scripts/world/act0_trigger_builder.gd
+++ scripts/world/act0_trigger_builder.gd
@@ -1,9 +1,14 @@
 class_name Act0TriggerBuilder
 extends Node3D
 
+const TEMPLE_GATE_SCRIPT := preload("res://scripts/world/temple_gate.gd")
+const FADED_SIGIL_SCRIPT := preload("res://scripts/world/faded_sigil_threshold.gd")
+const TEMPLE_INTERACTION_SCRIPT := preload("res://scripts/world/temple_interaction.gd")
+const CATACOMB_ROOM_SCRIPT := preload("res://scripts/world/catacomb_room_trigger.gd")
+
 func _ready()->void:
-	_gate(Act0Layout.TEMPLE_GATE_TRIGGER,"TempleGate","res://scripts/world/temple_gate.gd",Act0Layout.TEMPLE_GATE_TRIGGER_SIZE)
-	_gate(Act0Layout.FADED_SIGIL_TRIGGER,"FadedSigil","res://scripts/world/faded_sigil_threshold.gd",Act0Layout.FADED_SIGIL_TRIGGER_SIZE)
+	_gate(Act0Layout.TEMPLE_GATE_TRIGGER,"TempleGate",TEMPLE_GATE_SCRIPT,Act0Layout.TEMPLE_GATE_TRIGGER_SIZE)
+	_gate(Act0Layout.FADED_SIGIL_TRIGGER,"FadedSigil",FADED_SIGIL_SCRIPT,Act0Layout.FADED_SIGIL_TRIGGER_SIZE)
 	_interact(Act0Layout.KEEPER_TRIGGER,"Keeper","keeper",Act0Layout.KEEPER_TRIGGER_SIZE)
 	_interact(Act0Layout.COVENANT_TRIGGER,"Covenant","covenant",Act0Layout.COVENANT_TRIGGER_SIZE)
 	_interact(Act0Layout.SMITH_TRIGGER,"Smith","smith")
@@ -14,29 +19,29 @@ func _ready()->void:
 		_room(Act0Layout.catacomb_room_trigger_position(room),"CatacombRoom%d"%room,room)
 
 func _area(name:String,pos:Vector3,size:Vector3=Act0Layout.TEMPLE_INTERACTION_TRIGGER_SIZE)->Area3D:
-	var a:=Area3D.new()
-	a.name=name
-	a.position=pos
-	var c:=CollisionShape3D.new()
+	var area:=Area3D.new()
+	area.name=name
+	area.position=pos
+	var collision:=CollisionShape3D.new()
 	var shape:=BoxShape3D.new()
 	shape.size=size
-	c.shape=shape
-	a.add_child(c)
-	return a
+	collision.shape=shape
+	area.add_child(collision)
+	return area
 
-func _gate(pos:Vector3,name:String,script_path:String,size:Vector3)->void:
-	var a:=_area(name,pos,size)
-	a.set_script(load(script_path))
-	add_child(a)
+func _gate(pos:Vector3,name:String,script:Script,size:Vector3)->void:
+	var area:=_area(name,pos,size)
+	area.set_script(script)
+	add_child(area)
 
 func _interact(pos:Vector3,name:String,kind:String,size:Vector3=Act0Layout.TEMPLE_INTERACTION_TRIGGER_SIZE)->void:
-	var a:=_area(name,pos,size)
-	a.set_script(load("res://scripts/world/temple_interaction.gd"))
-	a.set("interaction",kind)
-	add_child(a)
+	var area:=_area(name,pos,size)
+	area.set_script(TEMPLE_INTERACTION_SCRIPT)
+	area.set("interaction",kind)
+	add_child(area)
 
 func _room(pos:Vector3,name:String,index:int)->void:
-	var a:=_area(name,pos,Act0Layout.CATACOMB_ROOM_TRIGGER_SIZE)
-	a.set_script(load("res://scripts/world/catacomb_room_trigger.gd"))
-	a.set("room",index)
-	add_child(a)
+	var area:=_area(name,pos,Act0Layout.CATACOMB_ROOM_TRIGGER_SIZE)
+	area.set_script(CATACOMB_ROOM_SCRIPT)
+	area.set("room",index)
+	add_child(area)
